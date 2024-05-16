e94ea085c61dce54b692b2a4987d66c288397990
Nithin <nithin@Nithins-MacBook-Pro.local>
2024-05-16 09:29:44 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Dashboard from './Dashboard';
+import Home from './pages/Home/Home'
 import { SnackbarProvider } from 'notistack';
 
 function App() {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <Dashboard />
+    <SnackbarProvider >
+      <div>
+        <Home />
+      </div>
     </SnackbarProvider>
   );
 }
